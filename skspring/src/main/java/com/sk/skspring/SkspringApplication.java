ff299bc54350905d619227f2eb47430db069730b
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2022-05-03 09:54:03 +0530

    Add App

---
+++ skspring/src/main/java/com/sk/skspring/SkspringApplication.java
@@ -0,0 +1,13 @@
+package com.sk.skspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkspringApplication.class, args);
+	}
+
+}
